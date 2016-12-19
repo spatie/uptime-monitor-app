1b5af49fa20dbc759b7e2eaad719eeeec4ca58dc
Freek Van der Herten <freek@spatie.be>
2016-12-20 00:33:19 +0100

    Add scheduled commands

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -24,8 +24,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('monitor:check-uptime')->everyMinute();
+        $schedule->command('monitor:check-certificate')->twiceDaily();
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('02:00');
     }
 
     /**
